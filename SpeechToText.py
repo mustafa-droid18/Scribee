ff9a52061dfec815ea515911ab27f913e17ba29a
asonawane39 <93039112+asonawane39@users.noreply.github.com>
2022-08-29 18:48:08 +0530

    Create SpeechToText.py

---
+++ SpeechToText.py
@@ -0,0 +1,77 @@
+#Code to convert Speech to text using AssemblyAI API
+#Audio file format is .mp3 and the output is retrieved in the form of a .txt file
+import requests
+import time
+API_KEY_ASSEMBLYAI = '31d08ebfe16243d1b87ae65e76d2d95c' #API key provided by AssemblyAI for access
+
+
+upload_endpoint = 'https://api.assemblyai.com/v2/upload'
+transcript_endpoint = 'https://api.assemblyai.com/v2/transcript'
+
+headers_auth_only = {'authorization': API_KEY_ASSEMBLYAI}
+
+headers = {
+    "authorization": API_KEY_ASSEMBLYAI,
+    "content-type": "application/json"
+}
+
+CHUNK_SIZE = 5_242_880  # 5MB
+
+
+def upload(filename):
+    def read_file(filename):
+        with open(filename, 'rb') as f:
+            while True:
+                data = f.read(CHUNK_SIZE)
+                if not data:
+                    break
+                yield data
+
+    upload_response = requests.post(upload_endpoint, headers=headers_auth_only, data=read_file(filename))
+    return upload_response.json()['upload_url']
+
+
+def transcribe(audio_url):
+    transcript_request = {
+        'audio_url': audio_url
+    }
+
+    transcript_response = requests.post(transcript_endpoint, json=transcript_request, headers=headers)
+    return transcript_response.json()['id']
+
+        
+def poll(transcript_id):
+    polling_endpoint = transcript_endpoint + '/' + transcript_id
+    polling_response = requests.get(polling_endpoint, headers=headers)
+    return polling_response.json()
+
+
+def get_transcription_result_url(url):
+    transcribe_id = transcribe(url)
+    while True:
+        data = poll(transcribe_id)
+        if data['status'] == 'completed':
+            return data, None
+        elif data['status'] == 'error':
+            return data, data['error']
+            
+        print("waiting for 20 seconds") #cooldown period of 20 seconds before the code sends data for conversion to the API again
+        time.sleep(20)
+        
+  #saving the transcribed text in a txt file      
+def save_transcript(url, title):
+    data, error = get_transcription_result_url(url)
+    
+    if data:
+        filename = title + '.txt'
+        with open(filename, 'w') as f:
+            f.write(data['text'])
+        print('Transcript saved')
+    elif error:
+        print("Error!!!", error)
+        
+   #converting test_audio.mp3 file
+filename = "test_audio.mp3"
+audio_url = upload(filename)
+
+save_transcript(audio_url, 'file_title')
