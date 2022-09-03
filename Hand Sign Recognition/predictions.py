5abf451482524d1f3b8d3729346e40e8a95cda83
Mustafa Poonawala <65003588+mustafa-droid18@users.noreply.github.com>
2022-09-04 01:33:24 +0530

    Final predictions file

---
+++ Hand Sign Recognition/predictions.py
@@ -0,0 +1,148 @@
+import cv2
+import numpy as np
+import os
+from matplotlib import pyplot as plt
+import time
+import mediapipe as mp # Go through Media pipe
+mp_holistic =mp.solutions.holistic # Bringing holsitic model
+mp_drawing =mp.solutions.drawing_utils # Drawing utilities
+def mediapipe_detection(image,model):
+    image =cv2.cvtColor(image,cv2.COLOR_BGR2RGB) # COLOR CONVERSION - BGR 2 RGB
+    image.flags.writeable= False                 # Image is no longer writeable (Helps in saving memory)
+    results=model.process(image)                 # Make Predictions
+    image.flags.writeable=True                   # Image is now writeable
+    image =cv2.cvtColor(image,cv2.COLOR_RGB2BGR) # COLOR CONVERSION - RGB 2 BGR
+    return image,results
+def draw_landmarks(image,results):
+    mp_drawing.draw_landmarks(image,results.face_landmarks,mp_holistic.FACEMESH_TESSELATION)
+    mp_drawing.draw_landmarks(image,results.pose_landmarks,mp_holistic.POSE_CONNECTIONS)
+    mp_drawing.draw_landmarks(image,results.left_hand_landmarks,mp_holistic.HAND_CONNECTIONS)
+    mp_drawing.draw_landmarks(image,results.right_hand_landmarks,mp_holistic.HAND_CONNECTIONS)
+# Does the same thing as the draw_landmarks function except helps in customizing the landmarks
+def draw_styled_landmarks(imag,results):
+    # Draw Face Connections
+    mp_drawing.draw_landmarks(image,results.face_landmarks,mp_holistic.FACEMESH_TESSELATION,
+                              mp_drawing.DrawingSpec(color=(80,110,10),thickness=1, circle_radius=1),
+                              mp_drawing.DrawingSpec(color=(80,256,121),thickness=1,circle_radius=1)                            
+                             )
+    mp_drawing.draw_landmarks(image,results.pose_landmarks,mp_holistic.POSE_CONNECTIONS,
+                              mp_drawing.DrawingSpec(color=(80,22,10),thickness=2, circle_radius=4),
+                              mp_drawing.DrawingSpec(color=(80,44,121),thickness=2,circle_radius=2)
+                             )
+    mp_drawing.draw_landmarks(image,results.left_hand_landmarks,mp_holistic.HAND_CONNECTIONS,
+                              mp_drawing.DrawingSpec(color=(80,110,10),thickness=2, circle_radius=4),
+                              mp_drawing.DrawingSpec(color=(256,256,121),thickness=2,circle_radius=2)
+                             )
+    mp_drawing.draw_landmarks(image,results.right_hand_landmarks,mp_holistic.HAND_CONNECTIONS,
+                              mp_drawing.DrawingSpec(color=(245,117,66),thickness=2, circle_radius=4),
+                              mp_drawing.DrawingSpec(color=(245,66,230),thickness=2,circle_radius=2)
+                             )
+# mp_drawing.draw_landmarks??
+def extract_keypoints(results):    
+    pose=np.array([[res.x,res.y,res.z,res.visibility] for res in results.pose_landmarks.landmark]).flatten() if results.pose_landmarks else np.zeros(132)
+    # Similarly for left hand and right hand
+    lh=np.array([[res.x,res.y,res.z] for res in results.left_hand_landmarks.landmark]).flatten() if results.left_hand_landmarks else np.zeros(21*3)  # The if statement is for when there is no reading for yout left hand
+    rh=np.array([[res.x,res.y,res.z] for res in results.right_hand_landmarks.landmark]).flatten() if results.right_hand_landmarks else np.zeros(21*3)  # The if statement is for when there is no reading for yout right hand
+    face=np.array([[res.x,res.y,res.z] for res in results.face_landmarks.landmark]).flatten() if results.face_landmarks else np.zeros(468*3)
+    return np.concatenate([pose,face,lh,rh]) # RETURNS ALL THE KEYPOINTS IN ONE 2D First 132 values is pose keypoints next is 468 value value of face keypoint and so on
+
+# Path for exported data,numpy arrays
+DATA_PATH = os.path.join('MP_DATA')
+
+# Actions that we try to detect
+actions = np.array(['Hello','Thank You'])
+
+# Thirty videos worth of data
+no_sequences = 30
+
+# Videos are going to be 30 frames in length
+sequence_length = 30
+
+from tensorflow import keras
+from keras.models import Sequential
+from keras.layers import LSTM, Dense
+from keras.callbacks import TensorBoard
+model=Sequential()
+model.add(LSTM(64,return_sequences=True,activation='relu',input_shape=(30,1662)))
+model.add(LSTM(128,return_sequences=True,activation='relu',))
+model.add(LSTM(64,return_sequences=False,activation='relu',))
+model.add(Dense(64,activation='relu'))
+model.add(Dense(32,activation='relu'))
+model.add(Dense(actions.shape[0],activation='softmax'))
+model.compile(optimizer='Adam',loss='categorical_crossentropy',metrics=['categorical_accuracy'])
+model.load_weights('action.h5')
+
+colors = [(245,117,16), (117,245,16), (16,117,245)]
+def prob_viz(res, actions, input_frame, colors):
+    output_frame = input_frame.copy()
+    for num, prob in enumerate(res):
+        cv2.rectangle(output_frame, (0,60+num*40), (int(prob*100), 90+num*40), colors[num], -1)
+        cv2.putText(output_frame, actions[num], (0, 85+num*40), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2, cv2.LINE_AA)
+        
+    return output_frame
+
+# 1. New detection variables
+sequence = []
+sentence = []
+threshold = 0.98
+predictions=[]
+
+cap = cv2.VideoCapture(0)
+# Set mediapipe model 
+with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
+    while cap.isOpened():
+
+        # Read feed
+        ret, frame = cap.read()
+
+        # Make detections
+        image, results = mediapipe_detection(frame, holistic)
+        print(results)
+        
+        # Draw landmarks
+        draw_styled_landmarks(image, results)
+        
+        # 2. Prediction logic
+        keypoints = extract_keypoints(results)
+#         sequence.insert(0,keypoints)
+#         sequence = sequence[:30]
+        sequence.append(keypoints)
+        sequence = sequence[-30:]
+        
+        if len(sequence) == 30:
+            res = model.predict(np.expand_dims(sequence, axis=0))[0]
+            print(actions[np.argmax(res)])
+            predictions.append(np.argmax(res))
+            
+            
+        #3. Viz logic
+            if np.unique(predictions[-10:])[0]==np.argmax(res): 
+                if res[np.argmax(res)] > threshold: 
+                    
+                    if len(sentence) > 0: 
+                        if actions[np.argmax(res)] != sentence[-1]:
+                            sentence.append(actions[np.argmax(res)])
+                    else:
+                        sentence.append(actions[np.argmax(res)])
+
+            if len(sentence) > 5: 
+                sentence = sentence[-5:]
+
+            if len(sentence) > 5: 
+                sentence = sentence[-5:]
+
+            # Viz probabilities
+            #image = prob_viz(res, actions, image, colors)
+            
+        cv2.rectangle(image, (0,0), (640, 40), (245, 117, 16), -1)
+        cv2.putText(image, ' '.join(sentence), (3,30), 
+                       cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+        
+        # Show to screen
+        cv2.imshow('OpenCV Feed', image)
+        cv2.waitKey(20)
+        # Break gracefully
+        if cv2.waitKey(10) & 0xFF == ord('q'):
+            break
+    cap.release()
+    cv2.destroyAllWindows()
